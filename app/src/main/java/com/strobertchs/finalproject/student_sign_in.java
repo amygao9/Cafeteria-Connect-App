9dfab33ff67f4674d39bec658e27f6c10fb1ba9b
pa6em3lz <janice.law18@ycdsbk12.ca>
2017-12-10 02:32:05 -0500

    Student sign in part 2: initiate instances and Firebase

--- app/src/main/java/com/strobertchs/finalproject/student_sign_in.java
+++ app/src/main/java/com/strobertchs/finalproject/student_sign_in.java
@@ -2,13 +2,34 @@ package com.strobertchs.finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class student_sign_in extends AppCompatActivity {
+
+    EditText editFirstName;
+    EditText editLastName;
+    EditText editEmail;
+    EditText editPassword;
+    Button btnSignIn;
+
+    // Initiate Firebase
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference table_user = database.getReference("User");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_sign_in);
 
-    }    
+        editFirstName = (EditText) findViewById(R.id.Name);
+        editLastName = (EditText) findViewById(R.id.LastName);
+        editEmail = (EditText) findViewById(R.id.EmailAddress);
+        editPassword = (EditText) findViewById(R.id.Password);
+        btnSignIn = (Button) findViewById(R.id.Signin_Button);
+
+    }
 }
