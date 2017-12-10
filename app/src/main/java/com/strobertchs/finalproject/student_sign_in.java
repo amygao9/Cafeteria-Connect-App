ba3cd3495a52fd0eb127e4a9aed09381fc0236dc
pa6em3lz <janice.law18@ycdsbk12.ca>
2017-12-10 02:35:08 -0500

    Student sign in part 3: initiate button click to register user info, update user class

--- app/src/main/java/com/strobertchs/finalproject/student_sign_in.java
+++ app/src/main/java/com/strobertchs/finalproject/student_sign_in.java
@@ -1,12 +1,18 @@
 package com.strobertchs.finalproject;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class student_sign_in extends AppCompatActivity {
 
@@ -31,5 +37,31 @@ public class student_sign_in extends AppCompatActivity {
         editPassword = (EditText) findViewById(R.id.Password);
         btnSignIn = (Button) findViewById(R.id.Signin_Button);
 
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final ProgressDialog mDialog = new ProgressDialog(student_sign_in.this);
+                mDialog.setMessage("Please wait a moment...");
+                mDialog.show();
+
+                table_user.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        mDialog.dismiss();
+
+                        User user = new User(editEmail.getText().toString(), editPassword.getText().toString());
+                        table_user.child(editLastName.getText().toString() + "," + editFirstName.getText().toString()).setValue(user);
+
+                        Toast.makeText(student_sign_in.this, "Registration successful!", Toast.LENGTH_SHORT).show();
+                        finish();
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
     }
 }
