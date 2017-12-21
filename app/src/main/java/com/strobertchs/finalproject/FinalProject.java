1bfa0e1fc5b3525189174194bd45e0b23dfed6f5
Amy Gao <Amy0127@Amys-MacBook-Pro.local>
2017-12-21 11:09:41 -0500

    add listview in content home

--- app/src/main/java/com/strobertchs/finalproject/FinalProject.java
+++ app/src/main/java/com/strobertchs/finalproject/FinalProject.java
@@ -1,13 +1,30 @@
 package com.strobertchs.finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class FinalProject extends AppCompatActivity {
+public class FinalProject extends AppCompatActivity implements View.OnClickListener {
+    Button buttonWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_project);
+
+
+        buttonWelcome = (Button) findViewById(R.id.button);
+        buttonWelcome.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent i;
+        i = new Intent(this, student_sign_in.class);
+        startActivity(i);
     }
 }
