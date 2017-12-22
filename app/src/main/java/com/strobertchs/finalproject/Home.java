be85f16555725d1b1bd631567db14c20480b3a3b
pa6em3lz <janice.law18@ycdsbk12.ca>
2017-12-22 00:53:47 -0500

    update code for Home java file

--- app/src/main/java/com/strobertchs/finalproject/Home.java
+++ app/src/main/java/com/strobertchs/finalproject/Home.java
@@ -57,7 +57,7 @@ public class Home extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        //Set user for home header
+        //Set user on home header
         View header = navigationView.getHeaderView(0);
         fullName = (TextView)findViewById(R.id.fullName);
         fullName.setText(savedUser.currentUser.getEmail());
