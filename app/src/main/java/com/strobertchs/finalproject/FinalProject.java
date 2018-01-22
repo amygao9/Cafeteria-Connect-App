5f9dabeb37dc12e93b5ed11ca391aedf22bb7ff4
pa6em3lz <janice.law18@ycdsbk12.ca>
2018-01-21 19:00:09 -0500

    update automatic sign in

--- app/src/main/java/com/strobertchs/finalproject/FinalProject.java
+++ app/src/main/java/com/strobertchs/finalproject/FinalProject.java
@@ -1,10 +1,19 @@
 package com.strobertchs.finalproject;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import io.paperdb.Paper;
 
 public class FinalProject extends AppCompatActivity {
     Button studentButton;
@@ -14,6 +23,13 @@ public class FinalProject extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_final_project);
+        //Initiate Paper
+        Paper.init(this);
+
+        final String firstName = Paper.book().read(savedUser.FIRSTNAME);
+        final String lastName = Paper.book().read(savedUser.LASTNAME);
+        final String username = Paper.book().read(savedUser.USER);
+        final String password = Paper.book().read(savedUser.PASSWORD);
 
         studentButton = (Button) findViewById(R.id.studentButton);
         staffButton = (Button) findViewById(R.id.staffButton);
@@ -21,12 +37,12 @@ public class FinalProject extends AppCompatActivity {
         studentButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (savedUser.currentUser == null) {
-                    Intent i = new Intent(FinalProject.this, student_sign_in.class);
+                if (firstName != null && lastName != null) {
+                    Intent i = new Intent(FinalProject.this, Home.class);
                     startActivity(i);
                 }
-                else {
-                    Intent i = new Intent(FinalProject.this, Home.class);
+                else{
+                    Intent i = new Intent(FinalProject.this, student_sign_in.class);
                     startActivity(i);
                 }
             }
@@ -37,6 +53,39 @@ public class FinalProject extends AppCompatActivity {
             public void onClick (View view) {
                 Intent i = new Intent(FinalProject.this, cafeteria_sign_in.class);
                 startActivity(i);
+            }
+        });
+
+        if(firstName != null && lastName != null)
+        {
+            if(!firstName.isEmpty() && !lastName.isEmpty()) {
+                autoLogin(username);
+            }
+        }
+    }
+
+    private void autoLogin(final String username) {
+        // Initiate Firebase
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        final DatabaseReference table_user = database.getReference("User");
+
+        final ProgressDialog mDialog = new ProgressDialog(FinalProject.this);
+        mDialog.setMessage("Please wait a moment...");
+        mDialog.show();
+
+        table_user.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(username).exists()) {
+                    mDialog.dismiss();
+                    Intent i = new Intent(FinalProject.this, Home.class);
+                    startActivity(i);
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
             }
         });
     }
--- app/src/main/java/com/strobertchs/finalproject/savedUser.java
+++ app/src/main/java/com/strobertchs/finalproject/savedUser.java
@@ -2,4 +2,10 @@ package com.strobertchs.finalproject;
 
 public class savedUser {
     public static User currentUser;
+
+    public static final String FIRSTNAME = "";
+    public static final String LASTNAME = "";
+    public static final String USER = "";
+    public static final String PASSWORD = "";
+
 }
