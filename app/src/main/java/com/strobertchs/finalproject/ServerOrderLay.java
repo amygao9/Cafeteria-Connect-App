ba5c92f498394ab9461e7e0a5bb57a9721cbeaaa
pa6em3lz <janice.law18@ycdsbk12.ca>
2018-01-25 18:54:12 -0500

    add order ready button, retrieve & output orders (in progress)

--- app/src/main/java/com/strobertchs/finalproject/ServerOrderLay.java
+++ app/src/main/java/com/strobertchs/finalproject/ServerOrderLay.java
@@ -3,13 +3,24 @@ package com.strobertchs.finalproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+
+import com.strobertchs.finalproject.model.Order;
+import com.strobertchs.finalproject.model.SavedUsers;
+import com.strobertchs.finalproject.model.CartItem;
 
 public class ServerOrderLay extends AppCompatActivity {
 
@@ -18,11 +29,18 @@ public class ServerOrderLay extends AppCompatActivity {
     List<String> listDataHeader;
     HashMap<String, List<String>> listDataChild;
     TextView orderTotal;
+    Button orderReady;
+
+    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    final DatabaseReference order = database.getReference("Orders");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_server_order_lay);
+
+        orderReady = (Button) findViewById(R.id.orderReady);
+        orderReady.setFocusable(false);
 
         // get the listview
         expListView = (ExpandableListView) findViewById(R.id.lvExp);
@@ -31,6 +49,7 @@ public class ServerOrderLay extends AppCompatActivity {
         prepareListData();
 
         listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+
 
         // setting list adapter
         expListView.setAdapter(listAdapter);
@@ -88,6 +107,13 @@ public class ServerOrderLay extends AppCompatActivity {
                 return false;
             }
         });
+
+        orderReady.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
     /*
@@ -97,10 +123,37 @@ public class ServerOrderLay extends AppCompatActivity {
         listDataHeader = new ArrayList<String>();
         listDataChild = new HashMap<String, List<String>>();
 
+        // Adding child data
+        order.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Order order = dataSnapshot.child(SavedUsers.ORDERNUM).getValue(Order.class);
+                listDataHeader.add(SavedUsers.ORDERNUM);
+                List<CartItem> currentOrder = Home.SHOPPING_CART.getCartItemList();
+                //List<CartItem> currentOrder = order.getCartItems();
+                List<String> orderNameList = new ArrayList<>();
+                //List<String> orderQuantityList = new ArrayList<>();
 
+                // Adding child data
+                for(int i = 0; i < currentOrder.size(); i++) {
+                    orderNameList.add(currentOrder.get(i).getProductName());
+                    //orderQuantityList.add(Integer.toString(currentOrder.get(i).getQuantity()));
+                }
+
+                listDataChild.put(listDataHeader.get(0), orderNameList); // Header, Child data
+
+                //orderTotal.setText(request.getTotal());
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         // Adding child data
-        listDataHeader.add("User 2940");
+        /*listDataHeader.add("User 2940");
         listDataHeader.add("User123");
         listDataHeader.add("User940");
 
@@ -122,6 +175,6 @@ public class ServerOrderLay extends AppCompatActivity {
 
         listDataChild.put(listDataHeader.get(0), User2940); // Header, Child data
         listDataChild.put(listDataHeader.get(1), User123);
-        listDataChild.put(listDataHeader.get(2), User940);
+        listDataChild.put(listDataHeader.get(2), User940);*/
     }
 }
